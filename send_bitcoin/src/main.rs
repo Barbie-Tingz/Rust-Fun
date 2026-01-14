2aa9ea4a0f667ceb70ecd8dc80b795b5691fa7e3
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-01-13 19:19:41 -0500

    Code is functional, but may need to add trim

--- send_bitcoin/src/main.rs
+++ send_bitcoin/src/main.rs
@@ -3,22 +3,38 @@ use std::io;
 
 // send a specified number of bitcoin
 fn send_bitcoin() {
-     println!("We will send {} Bitcoin!" ); 
+     println!("We will send some Bitcoin!\n" ); 
 
      // creating a vec! to parse through 
      let clients = vec!["Jill", "Mike", "Ike", "Moe"]; 
 
      for client in &clients{
-        print!("{}", client); 
+        print!("{} ", client); 
      }
      // allowing user to pick from a person from vector 
+
+     println!("\nWho would you like to send some bitcoin to?\n"); 
+
      let mut recipient = String::new(); 
-     io::stdin().read_line(&mut recipient); 
+     let _user_input = io::stdin().read_line(&mut recipient); 
+     
+     // parsing through the vector if the user writes a name from the list 
+     if clients.contains(&recipient.trim()){
+        println!("\nHow much coin do you want to send?"); 
+
+     let mut amount = String::new(); 
+     io::stdin().read_line(&mut amount);
+
+     println!("You have sent {} bitcoin to recipient {:?}", amount, recipient);
+
+     }else {
+        println!("Please select a recipient within your client list!");
+     }
 }
 
 // receive a random number of bitcoin and output amount of bitcoin received
 fn receive_bitcoin() {
-    let amount = rand::thread_rng().random_range(1..20); 
+    let amount = rand::rng().random_range(1..20); 
 
     println!("You just received {} Bitcoin!", amount)
 }
