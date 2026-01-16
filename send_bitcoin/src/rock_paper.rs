a425a45fdedb266d841aeae28c4a5eefdf423741
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-01-15 20:13:38 -0500

    The component function is functional and now we are working on the score function.

--- send_bitcoin/src/rock_paper.rs
+++ send_bitcoin/src/rock_paper.rs
@@ -3,24 +3,33 @@
 // when to quit. 
  
 use std::io; 
+use rand::Rng;
 
 
 // this function will be for the computer to randomly pick Rock, Paper, Scissors
 fn opponent() {
 
-    let options = vec!["Rock", "Paper", "Scissors"]; 
+    let comp_selection: u8 = rand::thread_rng().gen_range(1..=3); 
 
-    for option in &options {
-        print!("{} ", option); 
-    }
+    let opp_select = 
+    if comp_selection == 1 {
+        "Rock"
+    }else if comp_selection == 2 {
+        "Paper"
+    }else if comp_selection == 3 {
+        "Scissors"
+    }else{
+        "Invalid option"
+    };
 
+println!("Your opponent picked {}!", opp_select); 
 }
 
 // this function is to keep score of who is winning
 fn score() {
 
 }
-
+  
 // this function is used to say who won
 fn winner() {
 
