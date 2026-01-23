a334b3576d2486015f3ee4cf582d83098dd80985
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-01-22 19:18:45 -0500

    Pony Powers mini project

---
+++ send_bitcoin/src/pony_powers.rs
@@ -0,0 +1,2 @@
+// mini project to build on our knowledge of structs, enums and pattern matching
+// different ponies have different attributes and pony info will include (name, health, magic level, etc)
