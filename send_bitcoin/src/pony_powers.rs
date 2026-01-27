23db5e308167e4e1548c554f897759b8e65a6490
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-01-26 21:21:40 -0500

    Created a rough draft of pony_powers, please build on it and understand how the match and structs work together

--- send_bitcoin/src/pony_powers.rs
+++ send_bitcoin/src/pony_powers.rs
@@ -1,2 +1,28 @@
 // mini project to build on our knowledge of structs, enums and pattern matching
 // different ponies have different attributes and pony info will include (name, health, magic level, etc)
+
+struct PonyAbilities { 
+    name: PonyName, 
+    pony_type: PonyType, 
+    magic_points: u32, 
+}
+
+enum PonyType {
+    Earth, 
+    Pegasus, 
+    Unicorn, 
+}
+
+enum PonyName {
+    Fluttershy, 
+    RainbowDash, 
+    AppleJack, 
+}
+
+fn main() {
+
+    match PonyAbilities{name, pony_type, magic_points} {
+        PonyAbilities {name:PonyName::RainbowDash, pony_type:PonyType::Pegasus, magic_points: 89} => "Peg, Rainbow, 89 points",
+        PonyAbilities {name:PonyName::Fluttershy, pony_type:PonyType::Earth, magic_points: 16} => "Earth, Flutter, 16 points",
+    };
+}
