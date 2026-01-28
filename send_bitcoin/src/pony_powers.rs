3bac4463496a1fb78b187c5d67c2fca8e767f14f
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-01-27 19:43:39 -0500

    Stopping while I am ahead. This mini project was pretty filmsy in the structure and design. I didn't know where to start and what I wanted to return. Too vague. We are dropping and switching to the final project.

--- send_bitcoin/src/pony_powers.rs
+++ send_bitcoin/src/pony_powers.rs
@@ -19,10 +19,62 @@ enum PonyName {
     AppleJack, 
 }
 
-fn main() {
 
-    match PonyAbilities{name, pony_type, magic_points} {
-        PonyAbilities {name:PonyName::RainbowDash, pony_type:PonyType::Pegasus, magic_points: 89} => "Peg, Rainbow, 89 points",
-        PonyAbilities {name:PonyName::Fluttershy, pony_type:PonyType::Earth, magic_points: 16} => "Earth, Flutter, 16 points",
+fn create_earth() -> PonyAbilities {
+    let earth_pony = PonyAbilities {
+        name:PonyName::AppleJack, 
+        pony_type:PonyType::Earth, 
+        magic_points:89, 
     };
+    return earth_pony;
+}
+
+
+fn create_peg() -> PonyAbilities {
+    let pegasus_pony = PonyAbilities {
+    name:PonyName::RainbowDash, 
+    pony_type:PonyType::Pegasus, 
+    magic_points:400, 
+};
+return pegasus_pony;
+}
+
+fn create_unicorn()-> PonyAbilities {
+    let unicorn_pony = PonyAbilities {
+    name:PonyName::Fluttershy, 
+    pony_type:PonyType::Unicorn, 
+    magic_points: 254, 
+};
+return unicorn_pony
+}
+
+fn create_pony() -> &'static str{
+    let earth = create_earth(); 
+    let peg = create_peg(); 
+    let uni = create_unicorn(); 
+    
+    let earth_pony = match earth {
+        PonyAbilities {name: PonyName::AppleJack, pony_type: PonyType::Earth, magic_points: 89} =>
+        "Apple Jack, Earth, points: 89", 
+    }
+    earth_pony
+}
+
+
+
+fn create_pony() -> &'static str{
+    let earth = create_earth(); 
+    let peg = create_peg(); 
+    let uni = create_unicorn(); 
+    
+   let earth_pony =  match earth {
+        PonyAbilities {name: PonyName::AppleJack, pony_type: PonyType::Earth, magic_points: 89} =>
+        println! ("Apple Jack, Earth, points: 89"), 
+    }
+    earth_pony
+}
+
+
+fn main() {
+create_pony(); 
 }
